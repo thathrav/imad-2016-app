b0ab64928baae218b3f2114b98c142e4f18cb7d8
thathrav <revathi.thathrav@gmail.com>
2016-09-30 22:40:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,8 +10,7 @@ button.onclick=function()
                var counter=request.responseText();
                var span=document.getElementById('count');
                span.innerHTML=counter.toString();
-               
-           } 
+               } 
         }
     };
     };
